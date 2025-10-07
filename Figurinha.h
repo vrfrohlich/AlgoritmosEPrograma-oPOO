24761820d583879aa73b5de382a061fba1e14798
vitordduarte <vitordduarte05@gmail.com>
2025-10-06 21:28:30 -0300

    Update Figurinha.h

---
+++ Figurinha.h
@@ -0,0 +1,20 @@
+#ifndef FIGURINHA_H
+#define FIGURINHA_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Figurinha {
+    // Atributos
+private:
+    int numero;
+    string nome;
+    string conteudo;
+    int status;
+    int nroPagina;
+
+    // Métodos
+public:
+    type method(type);
+};
+#endif
