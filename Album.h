c53c8d9d9b2a4472c744540149be48e582f3204b
vitordduarte <vitordduarte05@gmail.com>
2025-10-06 21:19:59 -0300

    Update Album.h

---
+++ Album.h
@@ -0,0 +1,18 @@
+#ifndef ALBUM_H
+#define ALBUM_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Album {
+    // Atributos
+private:
+    Pagina[] paginas;
+    Figurinha[] figurinhas;
+    Troca[] requisicoesTrocas;
+
+    // Métodos
+public:
+    type method(type);
+};
+#endif
